80767aae028903f89864d892ce9c47a3b47cb923
Roukema <58312286+Roukema@users.noreply.github.com>
2019-12-17 11:40:32 +0100

    W3D2-2

---
+++ week 3/Dag 2/W3D2-2/script.js
@@ -0,0 +1,11 @@
+huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+  setTimeout(() => {
+    callback();
+  }, 3000);
+};
+
+klaarMetHuiswerk = () => {
+  console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+};
+huiswerkMaken("wiskunde", klaarMetHuiswerk);
