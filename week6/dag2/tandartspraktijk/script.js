dcb71302bd2a63c5eb3b9f7cbcb90a5712173186
Roukema <58312286+Roukema@users.noreply.github.com>
2020-01-22 16:59:49 +0100

    verschillende onderdelen uitgewerkt werkt nog niet goed samen

--- week6/dag2/tandartspraktijk/script.js
+++ week6/dag2/tandartspraktijk/script.js
@@ -5,28 +5,32 @@ const data = {
       name: "joost",
       surname: "tandjes",
       tel: 06 - 12345678,
-      mail: "tandje@tandjes.nl"
+      mail: "tandje@tandjes.nl",
+      sick: false
     },
     {
       id: 2,
       name: "jaap",
       surname: "tandjes",
       tel: 06 - 12345678,
-      mail: "tandje@tandjes.nl"
+      mail: "tandje@tandjes.nl",
+      sick: false
     },
     {
       id: 3,
       name: "jannes",
       surname: "tandjes",
       tel: 06 - 12345678,
-      mail: "tandje@tandjes.nl"
+      mail: "tandje@tandjes.nl",
+      sick: false
     },
     {
       id: 4,
       name: "jip",
       surname: "tandjes",
       tel: "0612345678",
-      mail: "tandje@tandjes.nl"
+      mail: "tandje@tandjes.nl",
+      sick: false
     }
   ],
   assistants: [
@@ -74,42 +78,41 @@ const data = {
   appointments: [
     {
       id: "1",
-      day: "monday",
+      day: "maandag",
       time: "9:00",
-      namePatient: "tandjeweg",
-      nameDentist: "tandjetrekker",
+      namePatient: "tandjeverwijderd",
+      nameDentist: "joost tandjes",
       nameAssistent: "tandjehelper"
     },
     {
       id: "2",
-      day: "monday",
+      day: "maandag",
       time: "9:00",
       namePatient: "tandjeweg",
-      nameDentist: "tandjetrekker",
+      nameDentist: "joost tandjes",
       nameAssistent: "tandjehelper"
     },
     {
       id: "3",
-      day: "monday",
+      day: "maandag",
       time: "9:00",
       namePatient: "tandjeweg",
-      nameDentist: "tandjetrekker",
+      nameDentist: "joost tandjes",
       nameAssistent: "tandjehelper"
     },
     {
       id: "4",
-      day: "monday",
+      day: "maandag",
       time: "9:00",
       namePatient: "tandjeweg",
-      nameDentist: "tandjetrekker",
+      nameDentist: "joost tandjes",
       nameAssistent: "tandjehelper"
     }
   ]
 };
-
+let newData = { ...data };
 const updateDentistName = (data, id, newName) => {
   // copy the old state
-  const newData = { ...data };
   newData["dentists"] = [...data["dentists"]];
   const index = data["dentists"].findIndex(dentist => dentist["id"] === id);
   const newDentist = { ...data["dentists"][index] };
@@ -121,16 +124,29 @@ const updateDentistName = (data, id, newName) => {
   // return the new state
   return newData;
 };
+const denstistIsSick = (newData, dentistName, newSick) => {
+  // copy the old stat
+  const index = newData["dentists"].findIndex(
+    dentist => dentist["name"] === dentistName
+  );
+  const newDentist = { ...newData["dentists"][index] };
 
-let newData = updateDentistName(data, 1, "Jaap");
-console.log("oude data", data);
-console.log("nieuwe data is:");
-console.log(newData);
+  // update the new state
+  newDentist["sick"] = newSick;
+  newData["dentists"][index] = newDentist;
+
+  // return the new state
+  return newData;
+};
+
+newData = updateDentistName(data, 1, "Jaap");
+// console.log("oude data", data);
+// console.log("nieuwe data is:");
+// console.log(newData);
 
 const addDentist = (data, newName, newSurname, newTel, newMail) => {
   // copy the old state
   const newId = data["dentists"].length + 1;
-  let newData = { ...data };
 
   newData["dentists"] = [...data["dentists"]];
   const newDentist = {
@@ -151,7 +167,6 @@ const addDentist = (data, newName, newSurname, newTel, newMail) => {
 const addPatient = (data, newName, newSurname, newTel, newMail, newYear) => {
   // copy the old state
   const newId = data["patients"].length + 1;
-  let newData = { ...data };
 
   newData["patients"] = [...data["patients"]];
   const newPatient = {
@@ -171,6 +186,167 @@ const addPatient = (data, newName, newSurname, newTel, newMail, newYear) => {
   return newData;
 };
 
+const addAppointment = (day, time, namePatient, nameDentist, nameAssistent) => {
+  // copy the old state
+
+  const newId = newData["appointments"].length + 1;
+
+  newData["appointments"] = [...newData["appointments"]];
+
+  const newAppointment = {
+    id: newId,
+    day: day,
+    time: time,
+    namePatient: namePatient,
+    nameDentist: nameDentist,
+    nameAssistent: nameAssistent
+  };
+
+  // update the new state
+
+  newData["appointments"].push(newAppointment);
+  // console.log("zijn er 50 act", newData);
+  // return the new state
+  return newData;
+};
+// 50 random appointment to start with
+
+const getRandomPatient = () => {
+  const person = data["patients"][Math.floor(Math.random() * 4)];
+  return `${person.name} ${person.surname}`;
+};
+const getRandomDentist = () => {
+  const person = data["dentists"][Math.floor(Math.random() * 4)];
+  return `${person.name} ${person.surname}`;
+};
+const getRandomAssistent = () => {
+  const person = data["assistants"][Math.floor(Math.random() * 2)];
+  return `${person.name} ${person.surname}`;
+};
+const getRandomDay = () => {
+  const days = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag"];
+  const randomDay = days[Math.floor(Math.random() * 5)];
+  return randomDay;
+};
+
+const getRandomTime = () => {
+  let goodTime = false;
+  let hour;
+  while (goodTime === false) {
+    hour = Math.floor(Math.random() * 24);
+    if (hour > 7 && hour < 19) {
+      goodTime = true;
+    }
+  }
+  return `${hour}:00u`;
+};
+
+const make50Appointments = () => {
+  for (i = 0; i < 50; i++) {
+    addAppointment(
+      getRandomDay(),
+      getRandomTime(),
+      getRandomPatient(),
+      getRandomDentist(),
+      getRandomAssistent()
+    );
+  }
+};
+const addNewAppointment = (
+  day,
+  time,
+  namePatient,
+  nameDentist,
+  nameAssistent
+) => {
+  const appointments = newData["appointments"];
+  // console.log(appointments[0]["nameDentist"], " : ", nameDentist);
+  // console.log(appointments[0]["day"], " : ", day);
+  // console.log(appointments[0]["time"], " : ", time);
+  if (
+    nameDentist === appointments[0]["nameDentist"] &&
+    day === appointments[0]["day"] &&
+    time === appointments[0]["time"]
+  ) {
+    alert("Tijd al bezet");
+  } else {
+    addAppointment(day, time, namePatient, nameDentist, nameAssistent);
+  }
+};
+
+// console.log(data);
+// console.log("nieuwe data is:");
+// console.log(newData);
+
+// console.log("appointments", data["appointments"]);
+
+//
+
+//maakt een list object
+const addAppointmentContainer = async data => {
+  let listnode = document.createElement("li");
+  let timenode = document.createElement("p");
+  let patientnode = document.createElement("p");
+  let dentistnode = document.createElement("p");
+  let assistentnode = document.createElement("p");
+  //change their properties
+  const fliterName = dentist => {
+    // console.log(
+    //   data["nameDentist"],
+    //   dentist["name"] + " " + dentist["surname"]
+    // );
+    if (dentist["name"] + " " + dentist["surname"] == data["nameDentist"]) {
+      return dentist;
+    }
+  };
+  const dentistInfo = newData["dentists"].filter(fliterName);
+  if (dentistInfo[0]["sick"] === true) {
+    listnode.className = "appointmentCanceled";
+  } else {
+    listnode.className = "appointment";
+  }
+
+  timenode.className = "time";
+  patientnode.className = "patient";
+  dentistnode.className = "dentist";
+  assistentnode.className = "assistent";
+
+  timenode.innerHTML = data["time"];
+  patientnode.innerHTML = data["namePatient"];
+  dentistnode.innerHTML = data["nameDentist"];
+  assistentnode.innerHTML = data["nameAssistent"];
+
+  listnode.innerHTML +=
+    timenode.outerHTML +
+    patientnode.outerHTML +
+    dentistnode.outerHTML +
+    assistentnode.outerHTML;
+  document.getElementsByClassName("dayview")[0].appendChild(listnode);
+
+  return listnode;
+};
+
+// stuurt array naar de list
+const addAppointmentToDom = async appointments => {
+  await appointments.forEach(async element => {
+    await addAppointmentContainer(element);
+    // console.log("hallo Element:", element);
+  });
+
+  //add listener to button, checkbutton
+  // addCheckboxChangeListener();
+};
+const removeAppointment = (data, appointmentId) => {
+  // copy the old state
+
+  data["appointments"] = [...data["appointments"]];
+
+  console.log(data["appointments"]);
+  newData = data["appointments"].filter(item => item.id != appointmentId);
+  console.log("data na filter", newData);
+  return newData;
+};
+
 // newData = updateDentistName(data, 1, "Jaap");
 newData = addDentist(
   data,
@@ -187,63 +363,18 @@ newData = addPatient(
   "toos@tandartspraktijkbvt.nl",
   "1993"
 );
-console.log(data);
-console.log("nieuwe data is:");
-console.log(newData);
+newData = denstistIsSick(data, "joost", true);
+addNewAppointment(
+  "maandag",
+  "9:00",
+  "eigenlijk wil ik niet",
+  "joost tandjes",
+  "doet ertoe"
+);
+removeAppointment(newData, 1);
+make50Appointments();
+addAppointmentToDom(newData["appointments"]);
 
-console.log("appointments", data["appointments"]);
-
-//
-
-//maakt een list object
-const addAppointmentContainer = async data => {
-  let listnode = document.createElement("li");
-  let timenode = document.createElement("p");
-  let patientnode = document.createElement("p");
-  let dentistnode = document.createElement("p");
-  let assistentnode = document.createElement("p");
-  //change their properties
-
-  listnode.className = "appointment";
-  timenode.className = "time";
-  patientnode.className = "patient";
-  dentistnode.className = "dentist";
-  assistentnode.className = "assistent";
-
-  timenode.innerHTML = data["time"];
-  patientnode.innerHTML = data["namePatient"];
-  dentistnode.innerHTML = data["nameDentist"];
-  assistentnode.innerHTML = data["nameAssistent"];
-
-  //   console.log(data.done);
-  // if (data.dentist.sick === true) {
-  //   checkBoxnode.classname = true;
-  // } else {
-  //   checkBoxnode.checked = false;
-  // }
-  listnode.innerHTML +=
-    timenode.outerHTML +
-    patientnode.outerHTML +
-    dentistnode.outerHTML +
-    assistentnode.outerHTML;
-  document.getElementsByClassName("dayview")[0].appendChild(listnode);
-
-  return listnode;
-};
-
-// stuurt array naar de list
-const addAppointmentToDom = async appointments => {
-  await appointments.forEach(async element => {
-    await addAppointmentContainer(element);
-    console.log("hallo Element:", element);
-  });
-
-  //add listener to button, checkbutton
-  // addCheckboxChangeListener();
-};
-
-const addToDom = () => {
-  addAppointmentToDom(data["appointments"]);
-};
+const addToDom = () => {};
 
 document.addEventListener("DOMContentLoaded", addToDom);
